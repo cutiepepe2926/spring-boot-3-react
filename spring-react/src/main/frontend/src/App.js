f45e9d6eeb3c54b03701dbec664595240416b7cb
jiyun-youm <yumjiyun1013@gmail.com>
2025-12-16 15:02:22 +0900

    bookdetail,bookregister front

--- spring-react/src/main/frontend/src/App.js
+++ spring-react/src/main/frontend/src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 
 import Main from "./components/Main";
 import Chat from "./components/Chat";
+import BookDetail from "./components/BookDetail";
+import BookRegister from "./components/BookRegister";
+
 
 import AuthLayout from "./components/AuthLayout"; // 새로 만들 파일
 
@@ -13,9 +16,12 @@ function App() {
                 {/* 기존 */}
                 <Route path="/" element={<Main />} />
                 <Route path="/chat" element={<Chat />} />
+                <Route path="/detail/:id" element={<BookDetail />} />
+                <Route path="/register" element={<BookRegister />} />
 
                 {/* Auth 영역: /auth/* 아래로 묶음 */}
                 <Route path="/auth/*" element={<AuthLayout />} />
+            
             </Routes>
     );
 }
